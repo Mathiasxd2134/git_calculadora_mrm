86be10c60bd14c7cf370c7cd7e6d4b62dd8ccf07
Mathiasxd2134 <mathias.rm.9@gmail.com>
2024-11-01 22:41:15 -0500

    codigo de la calculadora

--- calculadora.py
+++ calculadora.py
@@ -27,4 +27,39 @@ def logaritmo(a):
         return "Error: Entrada no válida para logaritmo."
     return math.log(a)
 
+def main():
+    print("Calculadora avanzada:")
+    print("1. Suma")
+    print("2. Resta")
+    print("3. Multiplicacion")
+    print("4. Division")
+    print("5. Potencia")
+    print("6. Raiz cuadrada")
+    print("7. Raiz logaritmo")
+
+    opcion = int(input("Elige una operación (1/2/3/4/5/6/7):"))
+    a = float(input("Ingresa el primer número: "))
+    b = None
+    if opcion in [1, 2, 3, 4, 5]:
+        b = float(input("Ingresa el segundo número: "))
+
+    if opcion == 1:
+        print("Resultado:", suma(a, b))
+    elif opcion == 2:
+        print("Resultado:", resta(a, b))
+    elif opcion == 3:
+        print("Resultado:", multiplicacion(a, b))
+    elif opcion == 4:
+        print("Resultado:", division(a, b))
+    elif opcion == 5:
+        print("Resultado:", potencia(a, b))
+    elif opcion == 6:
+        print("Resultado:", raiz_cuadrada(a))
+    elif opcion == 7:
+        print("Resultado:", logaritmo(a))
+    else:
+        print("Opcion no valida")
+
+if __name__ == "__main__":
+    main()
 
